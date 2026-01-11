96f0f52c2ebe8032555cfd1dc5b6e2b457be2e8e
codeleaded <gnu.c.reaper@gmail.com>
2026-01-12 00:30:32 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -2,8 +2,8 @@
 
 int main(){
     LibCrawler lc = LibCrawler_New("./inc");
-	LibCrawler_Find(&lc,"./src/WSEM_Cube.c");
-	//LibCrawler_Build(&lc);
+	LibCrawler_EntryFind(&lc,"./src/WSEM_Cube.h");
+	LibCrawler_Build(&lc);
 	LibCrawler_Free(&lc);
     return 0;
 }
